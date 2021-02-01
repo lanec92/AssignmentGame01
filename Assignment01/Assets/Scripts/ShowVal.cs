cf3dceb8f570ce6fa9ed29b747b0b43c6d5dc1a7
Lane <lanec92@gmail.com>
2021-02-01 12:49:04 -0600

    Final Version (Completed Game)

---
+++ Assignment01/Assets/Scripts/ShowVal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowVal : MonoBehaviour
+{
+    public Text percentageText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        percentageText = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    public void textUpdate(float value)
+    {
+        percentageText.text = Mathf.RoundToInt(value * 100) + "%";
+    }
+}
