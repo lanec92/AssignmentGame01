62ed4dc03c83811a3ca1e481cf9c1fd5ac72ab7b
Lane <lanec92@gmail.com>
2021-02-02 18:45:26 -0600

    Updated Version with working volume slider and accurate percentage

---
+++ Assignment01/Assets/Scripts/ShowValVol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowValVol : MonoBehaviour
+{
+    public Text percentageText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        percentageText = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    public void textUpdate(float value)
+    {
+        percentageText.text = Mathf.RoundToInt((value + 80) * 1.25f) + "%";
+    }
+}
